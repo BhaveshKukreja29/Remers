162a66952c2d0df06730dfba512fea85ab1f1030
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-04-18 23:54:54 +0530

    Update animated_text.dart

--- animated_text.dart
+++ animated_text.dart
@@ -1,8 +1,60 @@
 import 'package:flutter/material.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
 
-class AnimaText extends StatelessWidget {
+class AnimaText extends StatefulWidget {
   const AnimaText({Key? key}) : super(key: key);
+
+  @override
+  _AnimaTextState createState() => _AnimaTextState();
+}
+
+class _AnimaTextState extends State<AnimaText> {
+  final List<String> texts = [
+    'Never miss an update',
+    'Never forget anything',
+    'Never lose track',
+  ];
+
+  int currentIndex = 0;
+  bool isTyping = true;
+  String displayText = '';
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _animateText();
+    });
+  }
+
+  void _animateText() async {
+    if (isTyping) {
+      if (displayText.length < texts[currentIndex].length) {
+        await Future.delayed(const Duration(milliseconds: 55));
+        setState(() {
+          displayText = texts[currentIndex].substring(0, displayText.length + 1);
+        });
+      } else {
+        await Future.delayed(const Duration(milliseconds: 750));
+        setState(() {
+          isTyping = false;
+        });
+      }
+    } else {
+      if (displayText.isNotEmpty) {
+        await Future.delayed(const Duration(milliseconds: 35));
+        setState(() {
+          displayText = displayText.substring(0, displayText.length - 1);
+        });
+      } else {
+        await Future.delayed(const Duration(milliseconds: 50));
+        setState(() {
+          isTyping = true;
+          currentIndex = (currentIndex + 1) % texts.length;
+        });
+      }
+    }
+    _animateText();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,33 +62,13 @@ class AnimaText extends StatelessWidget {
       height: 210,
       child: SizedBox(
         width: 250,
-        child: AnimatedTextKit(
-          animatedTexts: [
-            TyperAnimatedText('Never miss an update',
-                textStyle: const TextStyle(
-                  fontSize: 60.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-                speed: const Duration(milliseconds: 60)),
-            TyperAnimatedText('Never forget anything',
-                textStyle: const TextStyle(
-                  fontSize: 60.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-                speed: const Duration(milliseconds: 60)),
-            TyperAnimatedText('Never lose track',
-                textStyle: const TextStyle(
-                  fontSize: 60.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-                speed: const Duration(milliseconds: 60)),
-
-          ],
-          repeatForever: true,
-          totalRepeatCount: 3, // Set the number of times the animation should repeat
+        child: Text(
+          displayText,
+          style: const TextStyle(
+            fontSize: 60.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
         ),
       ),
     );
