f414119876dadc0a5d8ea7c99030dda35b88e701
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-04-26 04:05:39 +0530

    Update LoginPage.dart

--- pages/LoginPage.dart
+++ pages/LoginPage.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:reminders/UI elements/animated_text.dart';
-import 'package:reminders/UI elements/humraTextfildu.dart';
-import 'package:reminders/UI elements/button.dart';
+import 'package:remers_final_build/UI elements/animated_text.dart';
+import 'package:remers_final_build/UI elements/humaraTextfildu.dart';
+import 'package:remers_final_build/UI elements/button.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({super.key});
@@ -14,49 +14,46 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final userEmail = TextEditingController();
   final userPassword = TextEditingController();
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>(); // Add this line
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   void signUserIn() async {
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const Center(
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.grey),
+          ),
+        );
+      },
+    );
+
     try {
       await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: userEmail.text,
         password: userPassword.text,
       );
+      Navigator.pop(context);
     } on FirebaseAuthException catch (e) {
-      if (e.code == 'user-not-found') {
-        EmailNotThere();
-      } else if (e.code == 'wrong-password') {
-        WrongPassword();
+
+      Navigator.pop(context);
+
+      if (e.code == 'invalid-credential') {
+        InvalidInfo();
       }
     }
   }
 
-  void EmailNotThere() {
+  void InvalidInfo() {
     showDialog(
-      context: _scaffoldKey.currentContext, // Change this line
+      context: context,
       builder: (context) {
         return const AlertDialog(
           backgroundColor: Colors.deepPurple,
           title: Center(
             child: Text(
-              'Incorrect Email',
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  void WrongPassword() {
-    showDialog(
-      context: _scaffoldKey.currentContext, // Change this line
-      builder: (context) {
-        return const AlertDialog(
-          backgroundColor: Colors.deepPurple,
-          title: Center(
-            child: Text(
-              'Incorrect Password',
+              'Invalid credentials',
               style: TextStyle(color: Colors.white),
             ),
           ),
@@ -75,27 +72,20 @@ class _LoginPageState extends State<LoginPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const SizedBox(height: 50),
-
               AnimaText(),
-
               const SizedBox(height: 40),
-
               Myfield(
                 controller: userEmail,
                 hintText: 'Your email',
                 obscureText: false,
               ),
-
               const SizedBox(height: 15),
-
               Myfield(
                 controller: userPassword,
                 hintText: 'Password',
                 obscureText: true,
               ),
-
               const SizedBox(height: 15),
-
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 28.0),
                 child: Row(
@@ -108,38 +98,35 @@ class _LoginPageState extends State<LoginPage> {
                   ],
                 ),
               ),
-
               const SizedBox(height: 20),
-
               MyButton(
                 onTap: signUserIn,
+                bolText: "Sign In",
               ),
-
               const SizedBox(height: 45),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   const Expanded(
                     child: Divider(
-                        thickness: 2.5,
-                      ),
+                      thickness: 2.5,
+                    ),
                   ),
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 10),
-                      child: Text(
-                          "Or Continue With",
-                        style: TextStyle(fontSize: 15, color: Colors.grey.shade700),
-                      ),
+                    child: Text(
+                      "Or Continue With",
+                      style: TextStyle(fontSize: 15, color: Colors.grey.shade700),
+                    ),
                   ),
                   const Expanded(
                     child: Divider(
                       thickness: 2.5,
                     ),
                   ),
-                    ],
-                  ),
+                ],
+              ),
               const SizedBox(height: 45),
-
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -150,21 +137,16 @@ class _LoginPageState extends State<LoginPage> {
                 ],
               ),
               const SizedBox(height: 30),
-
               const Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("Not a member?  ",
-                    style: TextStyle(
-                        fontSize: 15
-                    ),
+                  Text(
+                    "Not a member?  ",
+                    style: TextStyle(fontSize: 15),
                   ),
                   Text(
                     "Register Now!",
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 15
-                    ),
+                    style: TextStyle(color: Colors.blue, fontSize: 15),
                   )
                 ],
               )
@@ -175,4 +157,3 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
-
