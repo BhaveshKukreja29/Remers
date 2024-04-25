bbf757c24c5b01dcdbc2dd357095a9f482e4f489
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-04-26 04:03:46 +0530

    Update button.dart

--- UI elements/button.dart
+++ UI elements/button.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget{
   final void Function()? onTap;
+  final String bolText;
 
   const MyButton({
     super.key,
     required this.onTap,
+    required this.bolText,
   });
 
   @override
@@ -16,11 +18,11 @@ class MyButton extends StatelessWidget{
         padding: const EdgeInsets.all(25.0),
         margin: const EdgeInsets.symmetric(horizontal: 25.0),
         decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.circular(30.0)),
-        child: const Center(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(30.0)),
+        child: Center(
           child: Text(
-            "Sign In",
+            bolText,
             style: TextStyle(color: Colors.white, fontSize: 20.0),
           ),
         ),
